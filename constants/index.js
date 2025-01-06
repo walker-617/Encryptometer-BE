032f566e7a7495e4dc5fe8f647ae20341d919b3b
Divakar Mahanthi <mahanthidivakarnaidu@aesl.in>
2025-01-06 13:05:33 +0530

    converted hex to base64 to reduce size of response

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,7 @@
 const CRYPTO_ALGOS = {
-  AES: "aes",
-  CHA_CHA: "chacha",
+  AES_128_CBC: "aes-128-cbc",
+  AES_256_CBC: "aes-256-cbc",
+  CHA_CHA_20: "chacha20",
 };
 
 module.exports = { CRYPTO_ALGOS };
--- helpers/encrypt.js
+++ helpers/encrypt.js
@@ -2,35 +2,39 @@ const { CRYPTO_ALGOS } = require("../constants");
 const crypto = require("crypto");
 
 require("dotenv").config();
-const { AES_256_SECRET_KEY, AES_128_SECRET_KEY, CHA_CHA_20_SECRET_KEY } =
-  process.env;
-const AES128SecretKey = Buffer.from(AES_128_SECRET_KEY, "hex");
-const AES256SecretKey = Buffer.from(AES_256_SECRET_KEY, "hex");
-const ChaChaSecretKey = Buffer.from(CHA_CHA_20_SECRET_KEY, "hex");
+const {
+  AES_256_CBC_SECRET_KEY,
+  AES_128_CBC_SECRET_KEY,
+  CHA_CHA_20_SECRET_KEY,
+} = process.env;
+const AES128SecretKey = Buffer.from(AES_128_CBC_SECRET_KEY, "hex");
+const AES256SecretKey = Buffer.from(AES_256_CBC_SECRET_KEY, "hex");
+const ChaCha20SecretKey = Buffer.from(CHA_CHA_20_SECRET_KEY, "hex");
 
 function encryptChaCha20(text, key) {
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv("chacha20", key, iv);
-  let encryptedData = cipher.update(text, "utf8", "hex");
-  encryptedData += cipher.final("hex");
-  return { encryptedData, iv: iv.toString("hex") };
+  let encryptedData = cipher.update(text, "utf8", "base64");
+  encryptedData += cipher.final("base64");
+  return { encryptedData, iv: iv.toString("base64") };
 }
 
 function encryptAES(text, key, type) {
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(type, key, iv);
-  let encryptedData = cipher.update(text, "utf8", "hex");
-  encryptedData += cipher.final("hex");
-  return { encryptedData, iv: iv.toString("hex") };
+  let encryptedData = cipher.update(text, "utf8", "base64");
+  encryptedData += cipher.final("base64");
+  return { encryptedData, iv: iv.toString("base64") };
 }
 
 function encrypt(data, algorithm) {
   switch (algorithm) {
-    case CRYPTO_ALGOS.AES:
+    case CRYPTO_ALGOS.AES_128_CBC:
+      return encryptAES(data, AES128SecretKey, "aes-128-cbc");
+    case CRYPTO_ALGOS.AES_256_CBC:
       return encryptAES(data, AES256SecretKey, "aes-256-cbc");
-    case CRYPTO_ALGOS.CHA_CHA:
-      return encryptChaCha20(data, ChaChaSecretKey);
-      x;
+    case CRYPTO_ALGOS.CHA_CHA_20:
+      return encryptChaCha20(data, ChaCha20SecretKey);
     default:
       throw new Error("Algorithm not supported");
   }
