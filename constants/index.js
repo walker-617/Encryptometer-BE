d02d16c8e6de8f99e8877fadb8ec167afde5bc06
Atul Dwivedi <atuldwivedi@aesl.in>
2025-01-06 16:24:01 +0530

    Added AES-GCM and chacha20 for BE

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,8 @@ const CRYPTO_ALGOS = {
   AES_128_CBC: "aes-128-cbc",
   AES_256_CBC: "aes-256-cbc",
   CHA_CHA_20: "chacha20",
+  AES_128_GCM: "aes-128-gcm",
+  AES_256_GCM: "aes-256-gcm",
 };
 
 module.exports = { CRYPTO_ALGOS };
--- helpers/encrypt.js
+++ helpers/encrypt.js
@@ -5,19 +5,32 @@ require("dotenv").config();
 const {
   AES_256_CBC_SECRET_KEY,
   AES_128_CBC_SECRET_KEY,
+  AES_256_GCM_SECRET_KEY,
+  AES_128_GCM_SECRET_KEY,
   CHA_CHA_20_SECRET_KEY,
 } = process.env;
 const AES128SecretKey = Buffer.from(AES_128_CBC_SECRET_KEY, "hex");
 const AES256SecretKey = Buffer.from(AES_256_CBC_SECRET_KEY, "hex");
 const ChaCha20SecretKey = Buffer.from(CHA_CHA_20_SECRET_KEY, "hex");
+const AES128GCMSecretKey = Buffer.from(AES_128_GCM_SECRET_KEY, 'hex');
+const AES256GCMSecretKey = Buffer.from(AES_256_GCM_SECRET_KEY, 'hex');
 
 function encryptChaCha20(text, key) {
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv("chacha20", key, iv);
-  let encryptedData = cipher.update(text, "utf8", "base64");
-  encryptedData += cipher.final("base64");
-  return { encryptedData, iv: iv.toString("base64") };
+  const nonce = crypto.randomBytes(12); // 96-bit nonce for ChaCha20-Poly1305
+  const cipher = crypto.createCipheriv('chacha20-poly1305', key, nonce);
+  
+  let encryptedData = cipher.update(text, 'utf8', 'base64');
+  encryptedData += cipher.final('base64');
+  
+  const authTag = cipher.getAuthTag().toString('base64');
+  
+  return {
+    encryptedData,
+    iv: nonce.toString('base64'),
+    authTag
+  };
 }
+
 
 function encryptAES(text, key, type) {
   const iv = crypto.randomBytes(16);
@@ -25,6 +38,21 @@ function encryptAES(text, key, type) {
   let encryptedData = cipher.update(text, "utf8", "base64");
   encryptedData += cipher.final("base64");
   return { encryptedData, iv: iv.toString("base64") };
+}
+function encryptAESGCM(text, key, algorithm) {
+  const iv = crypto.randomBytes(12); // 96-bit nonce recommended for GCM
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  
+  let encryptedData = cipher.update(text, 'utf8', 'base64');
+  encryptedData += cipher.final('base64');
+  
+  const authTag = cipher.getAuthTag().toString('base64');
+  
+  return {
+    encryptedData,
+    iv: iv.toString('base64'),
+    authTag
+  };
 }
 
 function encrypt(data, algorithm) {
@@ -35,6 +63,10 @@ function encrypt(data, algorithm) {
       return encryptAES(data, AES256SecretKey, "aes-256-cbc");
     case CRYPTO_ALGOS.CHA_CHA_20:
       return encryptChaCha20(data, ChaCha20SecretKey);
+    case CRYPTO_ALGOS.AES_128_GCM:
+      return encryptAESGCM(data, AES128GCMSecretKey, "aes-128-gcm");
+    case CRYPTO_ALGOS.AES_256_GCM:
+      return encryptAESGCM(data, AES256GCMSecretKey, "aes-256-gcm");
     default:
       throw new Error("Algorithm not supported");
   }
